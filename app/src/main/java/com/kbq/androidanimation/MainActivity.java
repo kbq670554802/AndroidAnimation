a8e4a5009e6d4f2ee588d6639a5fc438aabf1101
KBQ <67554802@qq.com>
2016-04-27 15:05:27 +0800

    android动画例子完成

--- app/src/main/java/com/kbq/androidanimation/MainActivity.java
+++ app/src/main/java/com/kbq/androidanimation/MainActivity.java
@@ -1,14 +1,157 @@
 package com.kbq.androidanimation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView image;
+    private Button scale,rotate,translate,mix,alpha,continue_btn,continue_btn2,flash,move,change,layout,frame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //我做了修改
+
+        image = (ImageView) findViewById(R.id.image);
+        scale = (Button) findViewById(R.id.scale);
+        rotate = (Button) findViewById(R.id.rotate);
+        translate = (Button) findViewById(R.id.translate);
+        alpha = (Button) findViewById(R.id.alpha);
+        continue_btn = (Button) findViewById(R.id.continue_btn);
+        continue_btn2 = (Button) findViewById(R.id.continue_btn2);
+        flash = (Button) findViewById(R.id.flash);
+        move = (Button) findViewById(R.id.move);
+        change=(Button) findViewById(R.id.change);
+        layout=(Button) findViewById(R.id.layout);
+        frame=(Button) findViewById(R.id.frame);
+        scale.setOnClickListener(this);
+        rotate.setOnClickListener(this);
+        translate.setOnClickListener(this);
+        alpha.setOnClickListener(this);
+        continue_btn.setOnClickListener(this);
+        continue_btn2.setOnClickListener(this);
+        flash.setOnClickListener(this);
+        move.setOnClickListener(this);
+        change.setOnClickListener(this);
+        layout.setOnClickListener(this);
+        frame.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Animation loadAnimation;
+        switch (view.getId()) {
+            case R.id.scale: {
+                loadAnimation = AnimationUtils.loadAnimation(this, R.anim.scale);
+                image.startAnimation(loadAnimation);
+                break;
+            }
+
+            case R.id.rotate: {
+                loadAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+                image.startAnimation(loadAnimation);
+                break;
+            }
+
+            case R.id.translate: {
+
+                loadAnimation = AnimationUtils
+                        .loadAnimation(this, R.anim.translate);
+                image.startAnimation(loadAnimation);
+                break;
+            }
+
+            case R.id.continue_btn: {
+                loadAnimation = AnimationUtils
+                        .loadAnimation(this, R.anim.translate);
+                image.startAnimation(loadAnimation);
+                final Animation loadAnimation2 = AnimationUtils.loadAnimation(this,
+                        R.anim.rotate);
+                loadAnimation.setAnimationListener(new Animation.AnimationListener() {
+
+                    @Override
+                    public void onAnimationStart(Animation arg0) {
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation arg0) {
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation arg0) {
+                        image.startAnimation(loadAnimation2);
+                    }
+                });
+                break;
+            }
+
+            case R.id.continue_btn2: {
+                loadAnimation = AnimationUtils.loadAnimation(this,
+                        R.anim.continue_anim);
+                image.startAnimation(loadAnimation);
+                break;
+            }
+
+            case R.id.alpha: {
+                loadAnimation = AnimationUtils.loadAnimation(this, R.anim.alpha);
+                image.startAnimation(loadAnimation);
+                break;
+            }
+
+            case R.id.move: {
+                TranslateAnimation translate = new TranslateAnimation(-50, 50,
+                        0, 0);
+                translate.setDuration(1000);
+                translate.setRepeatCount(Animation.INFINITE);
+                translate.setRepeatMode(Animation.REVERSE);
+                image.startAnimation(translate);
+
+                break;
+            }
+
+            case R.id.flash: {
+
+                AlphaAnimation alphaAnimation = new AlphaAnimation(0.1f, 1.0f);
+                alphaAnimation.setDuration(100);
+                alphaAnimation.setRepeatCount(10);
+                //倒序重复REVERSE  正序重复RESTART
+                alphaAnimation.setRepeatMode(Animation.REVERSE);
+                image.startAnimation(alphaAnimation);
+
+                break;
+            }
+
+            case R.id.change:
+            {
+                Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.zoom_in,R.anim.zoom_out);
+                break;
+            }
+
+            case R.id.layout:
+            {
+                Intent intent=new Intent(MainActivity.this,ListActivity.class);
+                startActivity(intent);
+                break;
+            }
+
+            case R.id.frame:
+            {
+                image.setImageResource(R.drawable.anim_list);
+                break;
+
+            }
+
+        }
     }
 }
